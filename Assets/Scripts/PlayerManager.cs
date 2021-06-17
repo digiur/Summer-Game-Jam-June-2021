c02fa2cd39e236be5d19b0e499a53d6e8e37b553
Cesar Estrada <estrada.cesar15@gmail.com>
2021-06-16 20:45:37 -0600

    Cesar Adding PLayerManager, TestScene1, and editing GameManager

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] public GameObject ghost;
+    [SerializeField] public GameObject wind;
+    [SerializeField] public GameObject crack;
+
+    private GameObject player;
+    bool checkingProb = false;
+    int result = -1;
+
+        int windProb = 10;
+        int ghostProb = 10;
+        int iceProb = 10;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+         if(!checkingProb){
+             StartCoroutine(checkProbability());
+        }
+    }
+
+    //SPAWNING METHODS
+    private void SpawnWind(){
+
+    }
+
+    private void SpawnGhost(){
+        Debug.Log("SPAWNED GHOST");
+        Instantiate(ghost, new Vector3((player.transform.position.x + Random.Range(-7, 7)),
+                                                (player.transform.position.y), 
+                                                (player.transform.position.z + 10)), Quaternion.identity);
+    }
+
+    private void SpawnIce(){
+
+    }
+
+    //IEnumerators
+
+        public IEnumerator checkProbability(){
+        checkingProb = true;
+        bool runLoop = true;
+
+            while(runLoop){
+                result = GameManager.EventProbabilityCounter(windProb, ghostProb, iceProb);
+                Debug.Log(result);
+                if(result != -1){
+                    if(result == 0){
+                        //spawn wind
+                        windProb = 10;
+                        //Debug.Log("SPAWNED WIND");
+                    } else if(result == 1){
+                        //spawn ghost
+                        ghostProb = 10;
+                        SpawnGhost();
+                    } else if(result == 2){
+                        iceProb =10;
+                        //Debug.Log("SPAWNED ICE");
+                    }
+                    checkingProb = false;
+                    runLoop = false;
+                    yield return result;
+                } else{
+                    windProb--;
+                    ghostProb--;
+                    iceProb--;
+                    yield return new WaitForSeconds(1);
+                }
+            }
+        }
+
+}
